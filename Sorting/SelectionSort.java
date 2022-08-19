015f24b314502d4ed66c25372939aa24dbbdc685
jitmanew-tyagi <jitmanew@gmail.com>
2022-08-19 16:51:05 +0530

    sorting

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 5, 4, 8, 7, 6, 10, 0};
+        display(arr);
+        sort(arr);
+        display(arr);
+    }
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i ++) {
+            int min = i;
+            for(int j = i + 1; j < n; j ++) {
+                if(arr[j] < arr[min]) min = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void display(int[] arr) {
+        for(int i : arr) System.out.print(i + " ");
+        System.out.println();
+    }
+}

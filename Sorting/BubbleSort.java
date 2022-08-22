88afccfeb7ed449fdbe648fe84296758df205cc9
jitmanew-tyagi <jitmanew@gmail.com>
2022-08-22 15:39:44 +0530

    Sorting

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 5, 4, 8, 7, 6, 10, 0};
+        display(arr);
+        sort(arr);
+        display(arr);
+    }
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for(int i = 1; i < arr.length; i ++) {
+            for(int j = 0; j < n - i; j ++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void display(int[] arr) {
+        for(int i : arr) System.out.print(i + " ");
+        System.out.println();
+    }
+}
